694b329ad1002688109bb23c51b4c57a13be171f
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-01-28 21:38:32 +0100

    Ajout d'update dans le main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     objTiger = CArreraTiger("tigerConf.json")
     sortieFolder =  objTiger.loadEmplacementFile()
 
-    if (objTiger.loadDepots("https://arrera-software.fr/tigerFile.json")==True):
+    if (objTiger.loadDepots("https://arrera-software.fr/depots.json")==True):
         print("Depot chargé")
         print(objTiger.getSoftAvailable())
         if sortieFolder == False:
@@ -15,18 +15,23 @@ def main():
             print("Emplacement déjà enregistré")
         print(objTiger.getSoftInstall())
         print(objTiger.checkUpdate())
-        sortie = input("1.Installer\n2.Uninstaller\n#")
+        sortie = input("1.Installer\n2.Uninstaller\n3.Update\n#")
 
-        if sortie == "1":
-            soft = input("Nom du logiciel à installer: ")
-            if (soft != ""):
-                print(objTiger.install(soft))
-        else :
-            if sortie == "2":
+        match sortie:
+            case "1":
+                soft = input("Nom du logiciel à installer: ")
+                if (soft != ""):
+                    print(objTiger.install(soft))
+            case "2":
                 soft = input("Nom du logiciel à désinstaller: ")
                 if (soft != ""):
                     print(objTiger.uninstall(soft))
-
+            case "3":
+                print("Soft a mettre a jour : ")
+                print(objTiger.checkUpdate())
+                soft = input ("Logiciel a mettre a jour : ")
+                if (soft != ""):
+                    print(objTiger.update(soft))
 
 if __name__ == "__main__":
     main()
